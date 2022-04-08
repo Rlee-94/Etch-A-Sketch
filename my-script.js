03657b8c42b748b0beb32e13b3e9f4dc3b94258a
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-04-08 18:31:53 -0500

    Added Ability to Change Board Size
    -Created changeSize() function that will allow a user to enter a value
    between 2 and 100. This will then populate the correct amount of squares
    in the board. Otherwise, it will return an error.

--- my-script.js
+++ my-script.js
@@ -1,14 +1,28 @@
+
+//Creating dynamic board size
 function createBoard(boardSize) {
+let board = document.querySelector('.board');
+let squares = board.querySelectorAll('div');
+squares.forEach((div) => div.remove());
+board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
 
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`
-board.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`
+let amount = boardSize * boardSize
 
-for (let i =0; i<256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
-    board.insertAdjacentElement("beforeend", square)
+for (let i =0; i<amount; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement("beforeend", square);
   }
 }
 
-createBoard(16)
+createBoard(16);
+
+function changeSize(input) {
+    if(input >=2 && input <=100){
+        createBoard(input);
+    } else {
+        console.log('ERROR: Value must be between 2 and 100')
+    }
+    
+}
