4bee8800bfb999ddc100d8de2042d0d75c5bb7a7
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-04-08 18:16:09 -0500

    Created Board with placeholder Buttons
    -Figured out how to separate divs in index.html to contain the board.
    -Created Etch A Sketch Header followed by game board div and the Buttons
    -The last div will take a user input for the size of the board.
    
    -Styled CSS to display-Grid and set a width/height of 500px each.
    
    -my-script.js is creating and appending the div elements to the board.

---
+++ my-script.js
@@ -0,0 +1,14 @@
+function createBoard(boardSize) {
+
+let board = document.querySelector('.board')
+board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`
+board.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`
+
+for (let i =0; i<256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'blue'
+    board.insertAdjacentElement("beforeend", square)
+  }
+}
+
+createBoard(16)
